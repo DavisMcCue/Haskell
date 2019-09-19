1e198ebbc0aafa078e3ba0326aaeb2a0a0fdd79a
Noah <nbowe16@gmail.com>
2019-09-19 19:36:17 -0400

    part 4

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Davis McCue-dmccue
 
 Contents:
 lab3-pair-9.hs
-    
+    Employee-
+        data type with 3 types (Executive, Manager, Engineer)
+        each with salary, age, and name
+    isManager-
+        function which accepts an Employee and returns true if they are a manager, false if they are not
+    promote-
+        function which ranks up an Employee (Engineer->Manager->Executive)
+    main-
+        checks the functionality of the isManager function
 
  
--- lab3-pair-9.hs
+++ lab3-pair-9.hs
@@ -1,20 +1,20 @@
-data Employee = Executive 
-    | Manager 
-    | Engineer 
+data Employee = Executive {name::String, age::Int, salary::Float}
+    | Manager {name::String, age::Int, salary::Float}
+    | Engineer {name::String, age::Int, salary::Float}
     deriving (Show, Eq)
 
 isManager :: Employee -> Bool
-isManager (Executive) = False
-isManager (Manager) = True
-isManager (Engineer) = False
+isManager (Executive _ _ _) = False
+isManager (Manager _ _ _)   = True
+isManager (Engineer _ _ _)  = False
 
 promote :: Employee -> Employee
-promote (Engineer) = Manager
-promote (Manager) = Executive
-promote (Executive) = Executive 
+promote (Engineer _ _ _)  = (Manager "Jeff" 10 12.1)
+promote (Manager _ _ _)   = (Executive "Jeff" 10 12.1)
+promote (Executive _ _ _) = (Executive "Jeff" 10 12.1) 
 
 main:: IO () 
 main = do 
-       print (False == isManager Engineer)
-       print (False == isManager Executive)
-       print (True == isManager Manager)
+       print (False == isManager (Engineer "Jeff" 10 12.1))
+       print (False == isManager (Executive "Jeff" 10 12.1))
+       print (True  == isManager (Manager "Jeff" 10 12.1))
